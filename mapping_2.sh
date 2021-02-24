5384b9c3f9736485b6ebb92c7891ec5788ebe0a4
Gabriela Toomer <47196867+gabrielachavez2019@users.noreply.github.com>
2021-02-24 15:10:53 -0600

    Update mapping_2.sh

--- mapping_2.sh
+++ mapping_2.sh
@@ -43,13 +43,25 @@ names='Fem-dLAT Fem-WT Fem-U1
 
 for name in $names
  do
+   echo Read Quality Analysis "$name"
+   fastqc "$name"_F.fastq.gz "$name"_R.fastq.gz
+   cutadapt "$name"_F.fastq.gz "$name"_R.fastq.gz 
+   
    echo Starting mapping of "$name"  against the mouse genome
   
       tophat2 --num-threads 32 -o /scratch/gatoo/HSV1-mouse/output_"$name"  -G /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf --transcriptome-index /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/ --read-edit-dist 2 --read-gap-length 2 --read-mismatches 2 --read-realign-edit-dist 1 --library-type fr-unstranded --min-intron-length 50 --max-intron-length 300000 /scratch/gatoo/Mus_musculus/UCSC/mm10/Sequence/Bowtie2Index/genome /scratch/gatoo/HSV1-mouse/"$name"_F.fastq.gz /scratch/gatoo/HSV1-mouse/"$name"_R.fastq.gz
 
-   echo Starting mapping of "$name" against the virus
+      echo Starting mapping of "$name" against the virus
+      echo Starting mapping sample "$name" against the virus with bowtie2
 
-      tophat2 --num-threads 32 -o /scratch/gatoo/HSV1-mouse/output_HSV1_"$name" /scratch/gatoo/HSV1-mouse/HSV1 /scratch/gatoo/HSV1-mouse/"$name"_F.fastq.gz /scratch/gatoo/HSV1-mouse/"$name"_R.fastq.gz
+       bowtie2 -t -x ../HSV1-mouse/HSV1 -1 /scratch/gatoo/HSV1-mouse/"$name"_F.fastq.gz -2  /scratch/gatoo/HSV1-mouse/"$name"_R.fastq.gz -S mapped_"$name".sam
+       samtools flagstat mapped_"$name".sam > flagstat_"$name".txt
+       samtools view -h -F 4 mapped_"$name".sam | samtools view -bS > mapped_"$name".bam
+       #Look at the alignment using samtools
+       samtools view -b mapped_"$name".bam | samtools fillmd -e - /home/gatoo/HSV1-mouse/HSV1.fa | more
+
+echo Finishing sample "$name"
+      #tophat2 --num-threads 32 -o /scratch/gatoo/HSV1-mouse/output_HSV1_"$name" /scratch/gatoo/HSV1-mouse/HSV1 /scratch/gatoo/HSV1-mouse/"$name"_F.fastq.gz /scratch/gatoo/HSV1-mouse/"$name"_R.fastq.gz
 
     echo Finishing mapping "$name"
     
@@ -61,5 +73,32 @@ cufflinks --num-threads 32 -o /scratch/gatoo/HSV1-mouse/output_cuff_"$name" --fr
 cp  /scratch/gatoo/HSV1-mouse/output_cuff_"$name"/genes.fpkm_tracking "$name".txt
 
    echo $name counting: done!
-  done
+
+done
+
+echo Running cuffmerge on all samples
+
+######################################################
+#####Make sure you have an assemblies.txt file!! #####
+######################################################
+
+cuffmerge -g /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf -s /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 assemblies.txt
+#cuffmerge -g /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.gtf -s /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 assemblies-Male.txt
+
+##Run Cuffdiff by using the merged transcriptome assembly along with the BAM files from TopHat for each replicate
+#cuffdiff -o diff_out -b /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 -L "$name"-dLAT,"$name"-U1 -u merged_asm/merged.gtf \/scratch/gatoo/HSV1-mouse/output_"$name"-dLAT/accepted_hits.bam  \/scratch/gatoo/HSV1-mouse/output_"$name"-U1/accepted_hits.bam
+#cuffdiff --num-threads 32 -o diff_out_"$name" -b /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 -L "$name"-dLAT,"$name"-U1 -u merged_asm/merged.gtf /scratch/gatoo/HSV1-mouse/output_"$name"-dLAT/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_"$name"-U1/accepted_hits.bam
+
+###Run this to get the gene expression Male vs Female Specific condition (INDIVIDUALS) in this case dLAT vs WT
+names='Fem Male'
+for name in $names
+ do
+ echo Running cuffmerge on $name
+  cuffdiff --num-threads 32 -o diff_out_2_"$name" -b /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 -L "$name"-dLAT,"$name"-WT -u merged_asm/merged.gtf /scratch/gatoo/HSV1-mouse/output_"$name"-dLAT/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_"$name"-WT/accepted_hits.bam
+ done
+ 
+cuffdiff --num-threads 32 -o diff_out_test -b /scratch/gatoo/Mus_musculus/UCSC/mm10/Annotation/Genes/genes.fa -p 8 -L Fem-WT,Fem-U1,Fem-dLAT,Male-WT,Male-U1,Male-dLAT -u merged_asm/merged.gtf /scratch/gatoo/HSV1-mouse/output_Fem-WT/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_Fem-U1/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_Fem-dLAT/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_Male-WT/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_Male-U1/accepted_hits.bam /scratch/gatoo/HSV1-mouse/output_Male-dLAT/accepted_hits.bam
+#done
+
+
 echo All done
